35ff147da537936fe7fc26384bc8d1660747ece6
tsai <viabytsai@gmail.com>
2022-02-08 11:09:28 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
@@ -23,6 +24,10 @@ const routes: Array<RouteConfig> = [
     {
         path:'/Statistics',
         component:Statistics
+    },
+    {
+        path:'*',
+        component:NotFound
     }
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
