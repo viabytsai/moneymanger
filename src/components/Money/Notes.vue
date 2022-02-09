d9cbf0874ba19d35175aa1be4187b187d43e4331
tsai <viabytsai@gmail.com>
2022-02-09 14:51:57 +0800

    用TS写完了money中的types功能

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input placeholder="在这里输入文字" type="text">
+    </label>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  name: 'Notes'
+};
+</script>
+
+<style lang='scss' scoped>
+@import "~@/assets/style/helper.scss";
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  //不给高度，用input撑
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+
+  .name {
+    padding-right: 16px;
+  }
+
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background-color: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <ul class="types">
+      <li :class="type ==='-' && 'selected'"
+          @click="selectType('-')">支出
+      </li>
+      <li :class="type ==='+' && 'selected'"
+          @click="selectType('+')">收入</li>
+    </ul>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class Types extends Vue {
+  type = '-'; //'-'表示支出 '+'表示收入
+  selectType(type: string) {
+    if (type !== '-' && type !== '+') {
+      throw new Error('type is unknown');
+    }
+    this.type = type;
+  }
+
+}
+</script>
+
+<style lang='scss' scoped>
+@import "~@/assets/style/helper.scss";
+
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 4px;
+      width: 100%;
+      background: black;
+    }
+  }
+}
+</style>
