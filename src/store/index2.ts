a4abdfa4d696ceaaf2181cb40a137382a801e04a
tsai <viabytsai@gmail.com>
2022-02-12 21:39:26 +0800

    合并model中重复代码到store

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,31 +1,9 @@
-import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/tagListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-    // record store
-    recordList: recordListModel.fetch(),
-    createRecord: (record: RecordItem) => {
-        recordListModel.create(record);
-    },
-    // tag store
-    tagList: tagListModel.fetch(),
-    findTag(id: string) {
-        return this.tagList.filter(t => t.id === id)[0];//获取tags中和当前id相同id
-    },
-    createTag: (name: string) => {
-        const message = tagListModel.create(name);
-            if (message === 'duplicated') {
-                window.alert('标签名重复');
-            } else if (message === 'success') {
-                window.alert('成功');
-            }
-    },
-    removeTag: (id: string) => {
-        return tagListModel.remove(id);
-    },
-    updateTag: (id: string, name: string) => {
-        return tagListModel.update(id, name);
-    }
+    ...recordStore,
+    ...tagStore,
 };
-
+console.log(store);
 export default store;
