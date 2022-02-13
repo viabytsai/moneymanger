5b42f4d88f786441cb9ac78b8f718624954a9a98
tsai <viabytsai@gmail.com>
2022-02-13 20:20:47 +0800

    修复标签页面获取不到列表的bug

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -19,7 +19,7 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import FormItem from '@/components/Money/FormItem.vue';
 import Button from '@/components/Button.vue';
-import store from '@/store/index2';
+
 
 @Component({
   components: {Button, FormItem},
@@ -37,15 +37,15 @@ export default class EditLabel extends Vue {
   }
   updateTag(name:string){
     if(this.tag) {
-      store.updateTag(this.tag.id, name)
+      this.$store.commit('updateTag',{id:this.tag.id,name:name})
     }
   }
   remove(){
-    if(this.tag){
-      if(store.removeTag(this.tag.id)){
-        this.$router.back()
-      }else {window.alert('删除失败')}
-    }
+    // if(this.tag){
+    //   if(store.removeTag(this.tag.id)){
+    //     this.$router.back()
+    //   }else {window.alert('删除失败')}
+    // }
   }
   goBack(){
     this.$router.back()
