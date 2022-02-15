c9b7114afb31c272b82d16463aaf6a431ed1ec0b
tsai <viabytsai@gmail.com>
2022-02-15 10:12:56 +0800

    修复money界面的css被影响

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -47,8 +47,8 @@ export default class Money extends Vue {
 }
 </script>
 
-<style lang='scss'>
-.layout-content {
+<style lang='scss' scoped>
+::v-deep .layout-content {
   display: flex;
   flex-direction: column-reverse; //相应的html布局要倒过来
 }
